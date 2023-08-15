24b90bd6ac6a83f7fd26fe05b2a429ce596c4619
MetaMechanists <percy.baker.0@gmail.com>
2023-08-15 14:38:24 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/vehicles/SpatialForce.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/SpatialForce.java
@@ -1,8 +1,6 @@
 package org.metamechanists.aircraft.vehicles;
 
-import lombok.Getter;
 import org.joml.Vector3d;
 
 
-@Getter
 public record SpatialForce(Vector3d force, Vector3d relativeLocation) {}
