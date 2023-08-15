11ce0e686074bd02e08b721b8da922d27e32e179
MetaMechanists <percy.baker.0@gmail.com>
2023-08-16 00:01:04 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
@@ -31,7 +31,7 @@ public class Glider extends SlimefunItem {
 
     private static final Vector3d STARTING_VELOCITY = new Vector3d(0.0, 0.00001, 0.0); // must start off with some velocity to prevent NaN issues
     private static final Vector3d STARTING_ANGULAR_VELOCITY = new Vector3d(0.0, 0.0, 0.0); // roll, yaw, pitch
-    private static final Vector3d STARTING_ROTATION = new Vector3d(0.0, 0.0, 0.0); // roll, yaw, pitch
+    private static final Vector3d STARTING_ROTATION = new Vector3d(Math.PI / 8, 0.0, 0.0); // roll, yaw, pitch
 
     private static final double MASS = 2.0;
     private static final double MOMENT_OF_INERTIA = MASS * 1; // silly approximation
@@ -111,7 +111,7 @@ public class Glider extends SlimefunItem {
     }
     private static @NotNull DisplayGroup buildAircraft(final Location location) {
         final DisplayGroup displayGroup = new ModelBuilder()
-                .rotation(Math.PI / 8, 0.0, 0.0)
+                .rotation(STARTING_ROTATION.x, STARTING_ROTATION.y, STARTING_ROTATION.z)
                 .add("main", modelMain())
                 .add("wing_front_1", modelWingFront1())
                 .add("wing_front_2", modelWingFront2())
