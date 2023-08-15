8d10fd70b56a5d1407f992abf36ea0444b298885
MetaMechanists <percy.baker.0@gmail.com>
2023-08-16 00:58:04 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
@@ -33,7 +33,7 @@ public class Glider extends SlimefunItem {
     private static final Vector3d STARTING_ANGULAR_VELOCITY = new Vector3d(0.0, 0.0, 0.0); // roll, yaw, pitch
     private static final Vector3d STARTING_ROTATION = new Vector3d(Math.PI / 6, 0, -Math.PI / 6); // roll, yaw, pitch
 
-    private static final double MASS = 3;
+    private static final double MASS = 0.1;
     private static final double MOMENT_OF_INERTIA = MASS * 1; // silly approximation
 
     public static final SlimefunItemStack GLIDER = new SlimefunItemStack(
