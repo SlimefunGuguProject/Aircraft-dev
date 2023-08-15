ef7df2ce71afb723c0167b24288ccf4dfdc1bcdc
MetaMechanists <percy.baker.0@gmail.com>
2023-08-15 23:39:57 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/vehicles/AircraftSurface.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/AircraftSurface.java
@@ -46,7 +46,7 @@ public class AircraftSurface {
         // A = surface area facing airflow
         // V = aircraft velocity
         final double aircraftSpeed = aircraftVelocity.length();
-        final Vector3d dragDirection = new Vector3d(airflowVelocity).normalize().mul(-1);
+        final Vector3d dragDirection = new Vector3d(airflowVelocity).normalize();
         //final Vector3d dragDirection = normal;
         final Vector3d force = dragDirection.mul(dragDirection.dot(airflowVelocity) * 0.5 * dragCoefficient * AIR_DENSITY * getRelativeArea(airflowVelocity) * aircraftSpeed * aircraftSpeed);
         return new SpatialForce(force, relativeLocation, ForceType.DRAG);
@@ -74,7 +74,7 @@ public class AircraftSurface {
         // V = aircraft velocity
         final double aircraftSpeed = aircraftVelocity.length();
         final Vector3d perpendicularDirection = new Vector3d(airflowVelocity).cross(normal);
-        final Vector3d liftDirection = new Vector3d(perpendicularDirection).cross(airflowVelocity).normalize().mul(-1);
+        final Vector3d liftDirection = new Vector3d(perpendicularDirection).cross(airflowVelocity).normalize();
 
         final Vector3d force = liftDirection.mul(liftDirection.dot(airflowVelocity) * 0.5 * liftCoefficient * AIR_DENSITY * getRelativeArea(airflowVelocity) * aircraftSpeed * aircraftSpeed);
         return new SpatialForce(force, relativeLocation, ForceType.LIFT);
