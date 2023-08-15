4fa9bdcee119e7fd0f2b70167c3d17e1e653ffed
MetaMechanists <percy.baker.0@gmail.com>
2023-08-15 20:09:47 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/vehicles/SpatialForce.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/SpatialForce.java
@@ -11,9 +11,10 @@ public record SpatialForce(Vector3d force, Vector3d relativeLocation, ForceType 
         return new Vector3d(force).cross(relativeLocation);
     }
 
-    public ModelLine getForceLine() {
+    public ModelLine getForceLine(final Vector3d rotation) {
         final Vector3d adjustedForce = new Vector3d(force).mul(1000);
-        final Vector3f originFloat = new Vector3f((float)relativeLocation.x, (float)relativeLocation.y, (float)relativeLocation.z);
+        final Vector3d adjustedLocation = getLocation(rotation);
+        final Vector3f originFloat = new Vector3f((float)adjustedLocation.x, (float)adjustedLocation.y, (float)adjustedLocation.z);
         final Vector3f destinationFloat = new Vector3f(originFloat).add((float)adjustedForce.x, (float)adjustedForce.y, (float)adjustedForce.z);
         return new ModelLine()
                 .material(type.getMaterial())
@@ -27,7 +28,7 @@ public record SpatialForce(Vector3d force, Vector3d relativeLocation, ForceType 
         return relativeLocation.rotateX(rotation.x).rotateY(rotation.y).rotateZ(rotation.z);
     }
 
-    public String stringHash() {
+    public @NotNull String stringHash() {
         return relativeLocation.toString() + type.toString();
     }
 }
