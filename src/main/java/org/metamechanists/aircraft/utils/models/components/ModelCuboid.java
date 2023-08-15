cf8d2638bb2d19cb05fc8a15a6eb20c671cefe7f
MetaMechanists <percy.baker.0@gmail.com>
2023-08-15 21:54:03 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/utils/models/components/ModelCuboid.java
+++ src/main/java/org/metamechanists/aircraft/utils/models/components/ModelCuboid.java
@@ -20,6 +20,8 @@ import java.util.Set;
 
 @SuppressWarnings("unused")
 public class ModelCuboid implements ModelComponent {
+    private double AREA_SCALE = 0.005;
+
     private final BlockDisplayBuilder main = new BlockDisplayBuilder();
 
     private Vector3f location = new Vector3f();
@@ -119,7 +121,7 @@ public class ModelCuboid implements ModelComponent {
     private @NotNull AircraftSurface getSurface(
             final double dragCoefficient, final double liftCoefficient,
             final @NotNull Vector3d startingLocation, final double surfaceWidth, final double surfaceHeight) {
-        final double area = surfaceWidth * surfaceHeight;
+        final double area = surfaceWidth * surfaceHeight * AREA_SCALE;
         final Vector3d relativeLocation = startingLocation.rotateX(rotation.x).rotateY(rotation.y).rotateZ(rotation.z);
         final Vector3d normal = new Vector3d(relativeLocation).normalize();
         return new AircraftSurface(dragCoefficient, liftCoefficient, area, normal, new Vector3d(location).add(relativeLocation));
--- src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
@@ -33,7 +33,7 @@ public class Glider extends SlimefunItem {
     private static final Vector3d STARTING_ANGULAR_VELOCITY = new Vector3d(0.0, 0.0, 0.0); // roll, yaw, pitch
     private static final Vector3d STARTING_ROTATION = new Vector3d(0.0, 0.0, 0.0); // roll, yaw, pitch
 
-    private static final double MASS = 10.0;
+    private static final double MASS = 0.005;
     private static final double MOMENT_OF_INERTIA = MASS; // silly approximation
 
     public static final SlimefunItemStack GLIDER = new SlimefunItemStack(
