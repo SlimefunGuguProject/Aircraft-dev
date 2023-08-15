dd1c7c4c84da4428b10c5e3f2fd9052b2b8d2ee7
MetaMechanists <percy.baker.0@gmail.com>
2023-08-15 17:36:36 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/utils/models/components/ModelComponent.java
+++ src/main/java/org/metamechanists/aircraft/utils/models/components/ModelComponent.java
@@ -2,7 +2,6 @@ package org.metamechanists.aircraft.utils.models.components;
 
 import org.bukkit.Location;
 import org.bukkit.block.Block;
-import org.bukkit.entity.BlockDisplay;
 import org.bukkit.entity.Display;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Vector3d;
@@ -12,7 +11,7 @@ import org.joml.Vector3d;
  * Represents a single component of a model, composed of one or multiple Displays.
  */
 public interface ModelComponent {
-    Display build(@NotNull final Location origin, @NotNull final Vector3d modelRotation)
+    Display build(@NotNull final Location origin, @NotNull final Vector3d modelRotation);
     @SuppressWarnings("unused")
     Display build(@NotNull final Block block, @NotNull final Vector3d modelRotation);
 }
