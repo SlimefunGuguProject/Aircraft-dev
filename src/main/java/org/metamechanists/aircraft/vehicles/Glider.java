23d87c3027effcdab940177c59200d758841d894
MetaMechanists <percy.baker.0@gmail.com>
2023-08-20 17:37:22 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
@@ -24,17 +24,17 @@ import java.util.stream.Collectors;
 
 
 public class Glider extends SlimefunItem {
-    private static final double DRAG_COEFFICIENT_BODY = 0.03;
-    private static final double DRAG_COEFFICIENT_WING = 0.03;
-    private static final double LIFT_COEFFICIENT_BODY = 0.17;
-    private static final double LIFT_COEFFICIENT_WING = 0.17;
+    private static final double DRAG_COEFFICIENT_BODY = 0.15;
+    private static final double DRAG_COEFFICIENT_WING = 0.15;
+    private static final double LIFT_COEFFICIENT_BODY = 0.60;
+    private static final double LIFT_COEFFICIENT_WING = 0.60;
 
     private static final Vector3d STARTING_VELOCITY = new Vector3d(0.0, 0.00001, 0.0); // must start off with some velocity to prevent NaN issues
     private static final Vector3d STARTING_ANGULAR_VELOCITY = new Vector3d(0.0, 0.0, 0.0); // roll, yaw, pitch
     private static final Vector3d STARTING_ROTATION = new Vector3d(Math.PI / 4, 0, 0); // roll, yaw, pitch
 
     private static final double MASS = 0.005;
-    private static final double MOMENT_OF_INERTIA = MASS * 0.001; // silly approximation
+    private static final double MOMENT_OF_INERTIA = MASS * 0.01; // silly approximation
 
     public static final SlimefunItemStack GLIDER = new SlimefunItemStack(
             "ACR_GLIDER",
@@ -100,8 +100,8 @@ public class Glider extends SlimefunItem {
     private static ModelCuboid modelRudder() {
         return new ModelCuboid()
                 .material(Material.BLUE_CONCRETE)
-                .size(0.5F, 2.0F, 0.1F)
-                .location(-0.8F, 1.6F, 0.0F);
+                .size(0.5F, 1.0F, 0.1F)
+                .location(-0.8F, 0.6F, 0.0F);
     }
 
     private static void place(final @NotNull Block block) {
