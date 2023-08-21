e3d1b69fde77a908a46439c36ea47fbd9f38e8cc
MetaMechanists <percy.baker.0@gmail.com>
2023-08-22 00:57:06 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
@@ -17,6 +17,7 @@ import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Vector3d;
 import org.metamechanists.aircraft.utils.PersistentDataTraverser;
+import org.metamechanists.aircraft.utils.Utils;
 import org.metamechanists.aircraft.utils.id.simple.DisplayGroupId;
 import org.metamechanists.aircraft.utils.models.ModelBuilder;
 import org.metamechanists.aircraft.utils.models.components.ModelCuboid;
@@ -33,6 +34,9 @@ public class Glider extends SlimefunItem {
     private static final double DRAG_COEFFICIENT_WING = 0.30;
     private static final double LIFT_COEFFICIENT_BODY = 0.60;
     private static final double LIFT_COEFFICIENT_WING = 0.60;
+
+    private static final double MAX_CONTROL_SURFACE_ROTATION = Math.PI / 4;
+    private static final double CONTROL_SURFACE_ROTATION_RATE = Math.PI / 16;
 
     private static final Vector3d STARTING_VELOCITY = new Vector3d(1.0, 0.00001, 0.0); // must start off with some velocity to prevent NaN issues
     private static final Vector3d STARTING_ANGULAR_VELOCITY = new Vector3d(0.0, 0.0, 0.0); // roll, yaw, pitch
