013a17e4791c7b9fb2c727a717f1f61a69f3678b
MetaMechanists <percy.baker.0@gmail.com>
2023-08-22 01:59:04 +0100

    Thrust

--- src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
@@ -249,6 +249,7 @@ public class Glider extends SlimefunItem {
         group.getDisplays().get("wing_front_2").setTransformationMatrix(modelWingFront2().getMatrix(rotation));
         group.getDisplays().get("wing_back_1").setTransformationMatrix(modelWingBack1().getMatrix(rotation));
         group.getDisplays().get("wing_back_2").setTransformationMatrix(modelWingBack2().getMatrix(rotation));
+        group.getDisplays().get("tail").setTransformationMatrix(modelTail().getMatrix(rotation));
         group.getDisplays().get("aileron_1").setTransformationMatrix(modelAileron1(controlSurfaces.aileron1).getMatrix(rotation));
         group.getDisplays().get("aileron_2").setTransformationMatrix(modelAileron2(controlSurfaces.aileron2).getMatrix(rotation));
         group.getDisplays().get("elevator_1").setTransformationMatrix(modelElevator1(controlSurfaces.elevator1).getMatrix(rotation));
@@ -258,12 +259,16 @@ public class Glider extends SlimefunItem {
     private static @NotNull Set<SpatialForce> getForces(final Vector3d velocity, final Vector3d rotation, final @NotNull ControlSurfaces controlSurfaces) {
         final Set<SpatialForce> forces = new HashSet<>();
         forces.add(getWeightForce());
+        forces.add(getThrustForce(rotation));
         forces.addAll(getDragForces(rotation, velocity, controlSurfaces));
         forces.addAll(getLiftForces(rotation, velocity, controlSurfaces));
         return forces;
     }
     private static @NotNull SpatialForce getWeightForce() {
-        return new SpatialForce(new Vector3d(0, -10 * MASS, 0), new Vector3d(0, 0, 0), ForceType.WEIGHT);
+        return new SpatialForce(new Vector3d(0, -10 * MASS, 0), new Vector3d(0, 0, 0));
+    }
+    private static @NotNull SpatialForce getThrustForce(final @NotNull Vector3d rotation) {
+        return new SpatialForce(new Vector3d(100, 0, 0).rotateX(rotation.x).rotateY(rotation.y).rotateZ(rotation.z), new Vector3d(0, 0, 0));
     }
     private static Set<SpatialForce> getDragForces(final Vector3d rotation, final Vector3d velocity, final @NotNull ControlSurfaces controlSurfaces) {
         return getSurfaces(controlSurfaces).stream()
--- src/main/java/org/metamechanists/aircraft/vehicles/SpatialForce.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/SpatialForce.java
@@ -6,29 +6,8 @@ import org.joml.Vector3f;
 import org.metamechanists.aircraft.utils.models.components.ModelLine;
 
 
-public record SpatialForce(Vector3d force, Vector3d relativeLocation, ForceType type) {
+public record SpatialForce(Vector3d force, Vector3d relativeLocation) {
     public Vector3d getTorqueVector() {
         return new Vector3d(force).cross(relativeLocation);
-    }
-
-    public ModelLine getForceLine(final Vector3d rotation) {
-        final Vector3d adjustedForce = new Vector3d(force).mul(100);
-        final Vector3d adjustedLocation = getLocation(rotation);
-        final Vector3f originFloat = new Vector3f((float)adjustedLocation.x, (float)adjustedLocation.y, (float)adjustedLocation.z);
-        final Vector3f destinationFloat = new Vector3f(originFloat).add((float)adjustedForce.x, (float)adjustedForce.y, (float)adjustedForce.z);
-        return new ModelLine()
-                .material(type.getMaterial())
-                .brightness(15)
-                .from(originFloat)
-                .to(destinationFloat)
-                .thickness(0.12F);
-    }
-
-    public Vector3d getLocation(final @NotNull Vector3d rotation) {
-        return relativeLocation.rotateX(rotation.x).rotateY(rotation.y).rotateZ(rotation.z);
-    }
-
-    public @NotNull String stringHash() {
-        return relativeLocation.toString() + type.toString();
     }
 }
