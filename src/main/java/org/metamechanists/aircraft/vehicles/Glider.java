31b32f4d7e6873937c1110669f74103df20ec998
MetaMechanists <percy.baker.0@gmail.com>
2023-09-11 22:34:33 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
@@ -32,13 +32,13 @@ import java.util.stream.Collectors;
 public class Glider extends SlimefunItem {
     private static final double DRAG_COEFFICIENT_BODY = 0.30;
     private static final double DRAG_COEFFICIENT_WING = 0.30;
-    private static final double LIFT_COEFFICIENT_BODY = 1.50;
-    private static final double LIFT_COEFFICIENT_WING = 1.50;
+    private static final double LIFT_COEFFICIENT_BODY = 1.00;
+    private static final double LIFT_COEFFICIENT_WING = 1.00;
 
     private static final double MAX_CONTROL_SURFACE_ROTATION = Math.PI / 6;
     private static final double CONTROL_SURFACE_ROTATION_RATE = Math.PI / 16;
 
-    private static final Vector3d STARTING_VELOCITY = new Vector3d(2.0, 0.00001, 0.0); // must start off with some velocity to prevent NaN issues
+    private static final Vector3d STARTING_VELOCITY = new Vector3d(0.0, 0.00001, 0.0); // must start off with some velocity to prevent NaN issues
     private static final Vector3d STARTING_ANGULAR_VELOCITY = new Vector3d(0.0, 0.0, 0.0); // roll, yaw, pitch
     private static final Vector3d STARTING_ROTATION = new Vector3d(0, 0, 0); // roll, yaw, pitch
 
@@ -269,7 +269,7 @@ public class Glider extends SlimefunItem {
         return new SpatialForce(new Vector3d(0, -10 * MASS, 0), new Vector3d(0, 0, 0));
     }
     private static @NotNull SpatialForce getThrustForce(final @NotNull Vector3d rotation) {
-        return new SpatialForce(new Vector3d(0.0, 0, 0).rotateX(rotation.x).rotateY(rotation.y).rotateZ(rotation.z), new Vector3d(0, 0, 0));
+        return new SpatialForce(new Vector3d(0.5, 0, 0).rotateX(rotation.x).rotateY(rotation.y).rotateZ(rotation.z), new Vector3d(0, 0, 0));
     }
     private static Set<SpatialForce> getDragForces(final Vector3d rotation, final Vector3d velocity, final @NotNull ControlSurfaces controlSurfaces) {
         return getSurfaces(controlSurfaces).stream()
