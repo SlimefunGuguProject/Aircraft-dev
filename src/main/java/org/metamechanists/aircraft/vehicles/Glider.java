cbc094005b03a9de860c165aec0ab0054743a54f
MetaMechanists <percy.baker.0@gmail.com>
2023-09-11 22:30:55 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
@@ -30,15 +30,15 @@ import java.util.stream.Collectors;
 
 
 public class Glider extends SlimefunItem {
-    private static final double DRAG_COEFFICIENT_BODY = 0.60;
-    private static final double DRAG_COEFFICIENT_WING = 0.60;
-    private static final double LIFT_COEFFICIENT_BODY = 1.20;
-    private static final double LIFT_COEFFICIENT_WING = 1.20;
+    private static final double DRAG_COEFFICIENT_BODY = 0.30;
+    private static final double DRAG_COEFFICIENT_WING = 0.30;
+    private static final double LIFT_COEFFICIENT_BODY = 1.50;
+    private static final double LIFT_COEFFICIENT_WING = 1.50;
 
     private static final double MAX_CONTROL_SURFACE_ROTATION = Math.PI / 6;
     private static final double CONTROL_SURFACE_ROTATION_RATE = Math.PI / 16;
 
-    private static final Vector3d STARTING_VELOCITY = new Vector3d(0.5, 0.00001, 0.0); // must start off with some velocity to prevent NaN issues
+    private static final Vector3d STARTING_VELOCITY = new Vector3d(2.0, 0.00001, 0.0); // must start off with some velocity to prevent NaN issues
     private static final Vector3d STARTING_ANGULAR_VELOCITY = new Vector3d(0.0, 0.0, 0.0); // roll, yaw, pitch
     private static final Vector3d STARTING_ROTATION = new Vector3d(0, 0, 0); // roll, yaw, pitch
 
