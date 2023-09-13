4a5894cdec26a5d64a2f93e4acae0c455cfe1c6b
MetaMechanists <percy.baker.0@gmail.com>
2023-09-14 00:26:59 +0100

    Revert "oh no"
    This reverts commit 057f10f67088e8f76cde27ccad079ffe33a2b96a.

--- src/main/java/org/metamechanists/aircraft/utils/Utils.java
+++ src/main/java/org/metamechanists/aircraft/utils/Utils.java
@@ -53,14 +53,12 @@ public class Utils {
 
     public Vector3d rotate(final @NotNull Vector3d vector, final @NotNull Vector3d rotation) {
         //return new Vector3d(vector).rotateX((float) rotation.x).rotateY((float) rotation.y).rotateZ((float) rotation.z);
-        final Vector3d normalRotation = rotation.normalize();
-        return new Vector3d(vector).rotateAxis(rotation.length(), normalRotation.x, normalRotation.y, normalRotation.z);
+        return new Vector3d(vector).rotateAxis(rotation.length(), rotation.x, rotation.y, rotation.z);
     }
 
     public Vector3f rotate(final @NotNull Vector3f vector, final @NotNull Vector3d rotation) {
-        final Vector3d normalRotation = rotation.normalize();
         //return new Vector3f(vector).rotateX((float) rotation.x).rotateY((float) rotation.y).rotateZ((float) rotation.z);
-        return new Vector3f(vector).rotateAxis((float) rotation.length(), (float) normalRotation.x, (float) normalRotation.y, (float) normalRotation.z);
+        return new Vector3f(vector).rotateAxis((float) rotation.length(), (float) rotation.x, (float) rotation.y, (float) rotation.z);
     }
 
     public Matrix4f rotate(final @NotNull Vector3d rotation) {
