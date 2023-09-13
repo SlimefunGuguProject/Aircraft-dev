de399183c4c698e9e205d38c75f2d9ccd065f566
MetaMechanists <percy.baker.0@gmail.com>
2023-09-13 19:56:05 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/vehicles/ControlSurface.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/ControlSurface.java
@@ -16,7 +16,7 @@ public class ControlSurface {
     }
 
     public void adjust(final double amount) {
-        angle += Utils.clampToRange(angle + amount, -MAX_CONTROL_SURFACE_ROTATION, MAX_CONTROL_SURFACE_ROTATION);
+        angle = Utils.clampToRange(angle + amount, -MAX_CONTROL_SURFACE_ROTATION, MAX_CONTROL_SURFACE_ROTATION);
         modifiedLastTick = true;
     }
 
