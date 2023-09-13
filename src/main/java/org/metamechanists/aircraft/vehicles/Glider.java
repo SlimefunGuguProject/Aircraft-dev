f22d78d11a7eea921093eaddf4cb8c32d918b199
MetaMechanists <percy.baker.0@gmail.com>
2023-09-13 18:11:06 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
@@ -45,8 +45,8 @@ public class Glider extends SlimefunItem {
 
     private static final double MAX_VELOCITY = 50.0;
 
-    private static final double MASS = 1.0;
-    private static final double MOMENT_OF_INERTIA = MASS * 0.01; // silly approximation
+    private static final double MASS = 0.1;
+    private static final double MOMENT_OF_INERTIA = MASS * 0.1; // silly approximation
 
     public static final SlimefunItemStack GLIDER = new SlimefunItemStack(
             "ACR_GLIDER",
@@ -300,7 +300,7 @@ public class Glider extends SlimefunItem {
         return new SpatialForce("main", ForceType.WEIGHT, new Vector3d(0, -2.0 * MASS, 0), new Vector3d(0, 0, 0));
     }
     private static @NotNull SpatialForce getThrustForce(final @NotNull Vector3d rotation) {
-        return new SpatialForce("main", ForceType.THRUST, new Vector3d(8.0, 0, 0).rotateX(rotation.x).rotateY(rotation.y).rotateZ(rotation.z), new Vector3d(0, 0, 0));
+        return new SpatialForce("main", ForceType.THRUST, new Vector3d(0.8, 0, 0).rotateX(rotation.x).rotateY(rotation.y).rotateZ(rotation.z), new Vector3d(0, 0, 0));
     }
     private static Set<SpatialForce> getDragForces(final Vector3d rotation, final Vector3d velocity, final @NotNull ControlSurfaces controlSurfaces) {
         return getSurfaces(controlSurfaces).stream()
