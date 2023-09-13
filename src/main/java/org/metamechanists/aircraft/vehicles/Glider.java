836a71fed24cfdd935ec1fc83b1596e49b2f28e2
MetaMechanists <percy.baker.0@gmail.com>
2023-09-13 13:51:04 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
@@ -33,8 +33,8 @@ import java.util.stream.Collectors;
 public class Glider extends SlimefunItem {
     private static final double DRAG_COEFFICIENT_BODY = 0.30;
     private static final double DRAG_COEFFICIENT_WING = 0.30;
-    private static final double LIFT_COEFFICIENT_BODY = 0.20;
-    private static final double LIFT_COEFFICIENT_WING = 1.00;
+    private static final double LIFT_COEFFICIENT_BODY = 0.05;
+    private static final double LIFT_COEFFICIENT_WING = 2.00;
 
     private static final double MAX_CONTROL_SURFACE_ROTATION = Math.PI / 12;
     private static final double CONTROL_SURFACE_ROTATION_RATE = Math.PI / 256;
