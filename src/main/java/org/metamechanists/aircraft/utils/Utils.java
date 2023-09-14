aab639394ae58aebdec77d124aeb538bc4a02a3e
MetaMechanists <percy.baker.0@gmail.com>
2023-09-14 01:06:05 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/utils/Utils.java
+++ src/main/java/org/metamechanists/aircraft/utils/Utils.java
@@ -54,7 +54,7 @@ public class Utils {
 
     public Vector3d rotate(final @NotNull Vector3d vector, final @NotNull Vector3d rotation) {
         if (rotation.length() < 0.0001) {
-            return new Vector3d();
+            return vector;
         }
         return new Vector3d(vector).rotate(new Quaterniond().fromAxisAngleRad(new Vector3d(rotation.x, rotation.y, rotation.z), (float) rotation.length()));
         //return new Vector3d(vector).rotateX((float) rotation.x).rotateY((float) rotation.y).rotateZ((float) rotation.z);
@@ -62,7 +62,7 @@ public class Utils {
 
     public Vector3f rotate(final @NotNull Vector3f vector, final @NotNull Vector3d rotation) {
         if (rotation.length() < 0.0001) {
-            return new Vector3f();
+            return vector;
         }
         return new Vector3f(vector).rotate(new Quaternionf().fromAxisAngleRad(new Vector3f((float) rotation.x, (float) rotation.y, (float) rotation.z), (float) rotation.length()));
         //return new Vector3f(vector).rotateX((float) rotation.x).rotateY((float) rotation.y).rotateZ((float) rotation.z);
