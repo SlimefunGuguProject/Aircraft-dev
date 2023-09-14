075668e034c8a0d175d715e7d6bd88bfc0cf28af
MetaMechanists <percy.baker.0@gmail.com>
2023-09-14 17:43:02 +0100

    Revert "OH GOD"
    This reverts commit 15630cd3d737ce1e5e1ddcf0874af07afc8ec072.

--- src/main/java/org/metamechanists/aircraft/utils/models/ModelBuilder.java
+++ src/main/java/org/metamechanists/aircraft/utils/models/ModelBuilder.java
@@ -16,15 +16,9 @@ import java.util.Map;
  */
 public class ModelBuilder {
     private final Map<String, ModelComponent> components = new HashMap<>();
-    private Quaterniond rotation = new Quaterniond();
 
     public ModelBuilder add(@NotNull final String name, @NotNull final ModelComponent component) {
         components.put(name, component);
-        return this;
-    }
-
-    public ModelBuilder rotation(final double x, final double y, final double z) {
-        this.rotation = new Quaterniond().fromAxisAngleRad(new Vector3d(x, y, z).normalize(), new Vector3d(x, y, z).length());
         return this;
     }
 
@@ -35,7 +29,7 @@ public class ModelBuilder {
      */
     public DisplayGroup buildAtLocation(@NotNull final Location center) {
         final DisplayGroup group = new DisplayGroup(center.clone(), 0, 0);
-        components.forEach((name, component) -> group.addDisplay(name, component.build(center.clone(), rotation)));
+        components.forEach((name, component) -> group.addDisplay(name, component.build(center.clone(), new Quaterniond())));
         return group;
     }
     /**
@@ -45,7 +39,7 @@ public class ModelBuilder {
      */
     public DisplayGroup buildAtBlockCenter(@NotNull final Location blockLocation) {
         final DisplayGroup group = new DisplayGroup(blockLocation.clone(), 0, 0);
-        components.forEach((name, component) -> group.addDisplay(name, component.build(blockLocation.clone().toCenterLocation(), rotation)));
+        components.forEach((name, component) -> group.addDisplay(name, component.build(blockLocation.clone().toCenterLocation(), new Quaterniond())));
         return group;
     }
 }
