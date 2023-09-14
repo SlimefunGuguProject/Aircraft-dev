745c548ce0abb2ab82a3e3095b48a99eb64d298e
MetaMechanists <percy.baker.0@gmail.com>
2023-09-14 01:26:41 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
@@ -36,8 +36,8 @@ public class Glider extends SlimefunItem {
     private static final double LIFT_COEFFICIENT_BODY = 0.30;
     private static final double LIFT_COEFFICIENT_WING = 1.20;
 
-    public static final double MAX_CONTROL_SURFACE_ROTATION = Math.PI / 4;
-    private static final double CONTROL_SURFACE_ROTATION_RATE = Math.PI / 24;
+    public static final double MAX_CONTROL_SURFACE_ROTATION = Math.PI / 12;
+    private static final double CONTROL_SURFACE_ROTATION_RATE = Math.PI / 48;
 
     private static final Vector3d STARTING_VELOCITY = new Vector3d(0.0, 0.00001, 0.0); // must start off with some velocity to prevent NaN issues
     private static final Vector3d STARTING_ANGULAR_VELOCITY = new Vector3d(0.0, 0.0, 0.0); // roll, yaw, pitch
@@ -45,7 +45,7 @@ public class Glider extends SlimefunItem {
     private static final double MAX_VELOCITY = 50.0;
 
     private static final double MASS = 0.01;
-    private static final double MOMENT_OF_INERTIA = MASS * 2.0; // silly approximation
+    private static final double MOMENT_OF_INERTIA = MASS * 0.4; // silly approximation
 
     public static final SlimefunItemStack GLIDER = new SlimefunItemStack(
             "ACR_GLIDER",
