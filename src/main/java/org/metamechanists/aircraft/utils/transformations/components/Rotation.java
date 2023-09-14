1e8094600439d35a6c8c3db09e4907329de7436a
MetaMechanists <percy.baker.0@gmail.com>
2023-09-14 17:46:50 +0100

    OH GOD

--- src/main/java/org/metamechanists/aircraft/utils/transformations/components/Rotation.java
+++ src/main/java/org/metamechanists/aircraft/utils/transformations/components/Rotation.java
@@ -2,24 +2,26 @@ package org.metamechanists.aircraft.utils.transformations.components;
 
 import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix4f;
+import org.joml.Quaterniond;
 import org.joml.Quaternionf;
 import org.joml.Vector3d;
 import org.joml.Vector3f;
 
 
 public class Rotation implements TransformationMatrixComponent {
-    private final Vector3d rotation;
+    private final Quaterniond rotation;
 
     public Rotation(@NotNull final Vector3d rotation) {
-        this.rotation = rotation;
+        if (rotation.length() < 0.0001) {
+            this.rotation = new Quaterniond();
+            return;
+        }
+        this.rotation = new Quaterniond().fromAxisAngleRad(new Vector3d(rotation).normalize(), rotation.length());
     }
 
     @Override
     public void apply(@NotNull final Matrix4f matrix) {
-        if (rotation.length() < 0.0001) {
-            return;
-        }
-        matrix.rotate(new Quaternionf().fromAxisAngleRad((float) rotation.x, (float) rotation.y, (float) rotation.z, (float) rotation.length()));
+        matrix.rotate(new Quaternionf(rotation.x, rotation.y, rotation.z, rotation.w));
 //        matrix.rotateZ((float) rotation.z);
 //        matrix.rotateY((float) rotation.y);
 //        matrix.rotateX((float) rotation.x);
