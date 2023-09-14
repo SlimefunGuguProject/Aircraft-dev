22698e8846dea2c915960e21f3f5ea43db04e05c
MetaMechanists <percy.baker.0@gmail.com>
2023-09-14 16:25:50 +0100

    OH GOD

--- src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
@@ -44,7 +44,7 @@ public class Glider extends SlimefunItem {
 
     private static final Vector3d STARTING_VELOCITY = new Vector3d(0.0, 0.00001, 0.0); // must start off with some velocity to prevent NaN issues
     private static final Vector3d STARTING_ANGULAR_VELOCITY = new Vector3d(0.0, 0.0, 0.0); // roll, yaw, pitch
-    private static final Vector3d STARTING_ROTATION = new Vector3d(0, 0, 0); // roll, yaw, pitch
+    private static final Vector3d STARTING_ROTATION = new Vector3d(0.2, 0, 0); // roll, yaw, pitch
     private static final double MAX_VELOCITY = 50.0;
 
     private static final double MASS = 0.01;
