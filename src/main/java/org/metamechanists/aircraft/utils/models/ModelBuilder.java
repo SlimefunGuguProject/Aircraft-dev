8d70f7d9ecdcf365d03a9cb0a4be8912ba119f95
MetaMechanists <percy.baker.0@gmail.com>
2023-09-14 17:43:03 +0100

    Revert "OH GOD"
    This reverts commit 68a47aa818965874122f52adedfccaa7cebb3f14.

--- src/main/java/org/metamechanists/aircraft/utils/models/ModelBuilder.java
+++ src/main/java/org/metamechanists/aircraft/utils/models/ModelBuilder.java
@@ -2,7 +2,6 @@ package org.metamechanists.aircraft.utils.models;
 
 import org.bukkit.Location;
 import org.jetbrains.annotations.NotNull;
-import org.joml.Quaterniond;
 import org.joml.Vector3d;
 import org.metamechanists.aircraft.utils.models.components.ModelComponent;
 import org.metamechanists.metalib.sefilib.entity.display.DisplayGroup;
@@ -16,9 +15,15 @@ import java.util.Map;
  */
 public class ModelBuilder {
     private final Map<String, ModelComponent> components = new HashMap<>();
+    private Vector3d rotation = new Vector3d();
 
     public ModelBuilder add(@NotNull final String name, @NotNull final ModelComponent component) {
         components.put(name, component);
+        return this;
+    }
+    
+    public ModelBuilder rotation(final double x, final double y, final double z) {
+        this.rotation = new Vector3d(x, y, z);
         return this;
     }
 
@@ -29,7 +34,7 @@ public class ModelBuilder {
      */
     public DisplayGroup buildAtLocation(@NotNull final Location center) {
         final DisplayGroup group = new DisplayGroup(center.clone(), 0, 0);
-        components.forEach((name, component) -> group.addDisplay(name, component.build(center.clone(), new Quaterniond())));
+        components.forEach((name, component) -> group.addDisplay(name, component.build(center.clone(), rotation)));
         return group;
     }
     /**
@@ -39,7 +44,7 @@ public class ModelBuilder {
      */
     public DisplayGroup buildAtBlockCenter(@NotNull final Location blockLocation) {
         final DisplayGroup group = new DisplayGroup(blockLocation.clone(), 0, 0);
-        components.forEach((name, component) -> group.addDisplay(name, component.build(blockLocation.clone().toCenterLocation(), new Quaterniond())));
+        components.forEach((name, component) -> group.addDisplay(name, component.build(blockLocation.clone().toCenterLocation(), rotation)));
         return group;
     }
 }
