68a47aa818965874122f52adedfccaa7cebb3f14
MetaMechanists <percy.baker.0@gmail.com>
2023-09-14 17:38:55 +0100

    OH GOD

--- src/main/java/org/metamechanists/aircraft/utils/models/ModelBuilder.java
+++ src/main/java/org/metamechanists/aircraft/utils/models/ModelBuilder.java
@@ -2,6 +2,7 @@ package org.metamechanists.aircraft.utils.models;
 
 import org.bukkit.Location;
 import org.jetbrains.annotations.NotNull;
+import org.joml.Quaterniond;
 import org.joml.Vector3d;
 import org.metamechanists.aircraft.utils.models.components.ModelComponent;
 import org.metamechanists.metalib.sefilib.entity.display.DisplayGroup;
@@ -15,15 +16,9 @@ import java.util.Map;
  */
 public class ModelBuilder {
     private final Map<String, ModelComponent> components = new HashMap<>();
-    private Vector3d rotation = new Vector3d();
 
     public ModelBuilder add(@NotNull final String name, @NotNull final ModelComponent component) {
         components.put(name, component);
-        return this;
-    }
-    
-    public ModelBuilder rotation(final double x, final double y, final double z) {
-        this.rotation = new Vector3d(x, y, z);
         return this;
     }
 
@@ -34,7 +29,7 @@ public class ModelBuilder {
      */
     public DisplayGroup buildAtLocation(@NotNull final Location center) {
         final DisplayGroup group = new DisplayGroup(center.clone(), 0, 0);
-        components.forEach((name, component) -> group.addDisplay(name, component.build(center.clone(), rotation)));
+        components.forEach((name, component) -> group.addDisplay(name, component.build(center.clone(), new Quaterniond())));
         return group;
     }
     /**
@@ -44,7 +39,7 @@ public class ModelBuilder {
      */
     public DisplayGroup buildAtBlockCenter(@NotNull final Location blockLocation) {
         final DisplayGroup group = new DisplayGroup(blockLocation.clone(), 0, 0);
-        components.forEach((name, component) -> group.addDisplay(name, component.build(blockLocation.clone().toCenterLocation(), rotation)));
+        components.forEach((name, component) -> group.addDisplay(name, component.build(blockLocation.clone().toCenterLocation(), new Quaterniond())));
         return group;
     }
 }
