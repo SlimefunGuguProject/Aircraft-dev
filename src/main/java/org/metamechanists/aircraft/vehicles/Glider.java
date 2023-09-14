0a26ae86d2914e7422b29811ae5b0067348a9a03
MetaMechanists <percy.baker.0@gmail.com>
2023-09-14 19:00:06 +0100

    OH GOD

--- src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Glider.java
@@ -47,7 +47,7 @@ public class Glider extends SlimefunItem {
     private static final double MAX_VELOCITY = 50.0;
 
     private static final double MASS = 0.01;
-    private static final double MOMENT_OF_INERTIA = MASS; // silly approximation
+    private static final double MOMENT_OF_INERTIA = MASS * 0.3; // silly approximation
 
     public static final SlimefunItemStack GLIDER = new SlimefunItemStack(
             "ACR_GLIDER",
