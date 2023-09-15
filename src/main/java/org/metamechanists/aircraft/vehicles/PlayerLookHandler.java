2cfecfc24fdec574d8f1507d6a31a1c0bb166283
MetaMechanists <percy.baker.0@gmail.com>
2023-09-16 00:59:55 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/vehicles/PlayerLookHandler.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/PlayerLookHandler.java
@@ -12,13 +12,17 @@ import static java.lang.Math.toDegrees;
 
 @UtilityClass
 public class PlayerLookHandler {
+    private byte degreeToByte(final double degree) {
+        return (byte) (degree * 256.0F / 360.0F);
+    }
+
     public void adjustRotation(final @NotNull Player player, final double deltaYaw, final double deltaPitch) {
         final ProtocolManager manager = ProtocolLibrary.getProtocolManager();
         final PacketContainer packet = new PacketContainer(Server.ENTITY_HEAD_ROTATION);
         packet.getIntegers().write(0, player.getEntityId());
-        packet.getDoubles()
-                .write(0, player.getLocation().getYaw() + toDegrees(deltaYaw))
-                .write(1, player.getLocation().getPitch() + toDegrees(deltaPitch));
+        packet.getBytes()
+                .write(0, degreeToByte(player.getLocation().getYaw() + toDegrees(deltaYaw)))
+                .write(1, degreeToByte(player.getLocation().getPitch() + toDegrees(deltaPitch)));
         packet.getBooleans().write(0, false);
         manager.broadcastServerPacket(packet);
     }
