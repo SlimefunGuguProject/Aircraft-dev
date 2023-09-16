ec650a35baf2b4a042f9c81997a40713d561cc3a
MetaMechanists <percy.baker.0@gmail.com>
2023-09-16 15:10:19 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/Aircraft.java
+++ src/main/java/org/metamechanists/aircraft/Aircraft.java
@@ -6,6 +6,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.metamechanists.aircraft.items.Groups;
+import org.metamechanists.aircraft.vehicles.PlayerLookHandler;
 import org.metamechanists.aircraft.vehicles.VehicleTicker;
 import org.metamechanists.aircraft.vehicles.WasdHandler;
 
@@ -16,6 +17,7 @@ public final class Aircraft extends JavaPlugin implements SlimefunAddon {
 
     private static void initializeListeners() {
         WasdHandler.addProtocolListener();
+        PlayerLookHandler.addProtocolListener();
     }
 
     private void initializeRunnables() {
