e69c3fcd1c0daf9d742fa483927583fab3ab25c9
MetaMechanists <percy.baker.0@gmail.com>
2023-09-17 01:15:29 +0100

    oh no

--- src/main/java/org/metamechanists/aircraft/vehicles/PlayerLookHandler.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/PlayerLookHandler.java
@@ -5,32 +5,16 @@ import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.PacketContainer;
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 public class PlayerLookHandler {
     private static final ProtocolManager MANAGER = ProtocolLibrary.getProtocolManager();
 
-    public static void sendYawPacket(Player player, double deltaDegrees) {
-        final Location eyeLocation = player.getEyeLocation();
-        sendRotationPacket(player, eyeLocation.getYaw() + deltaDegrees, eyeLocation.getPitch());
-    }
-
-    public static void sendPitchPacket(Player player, double deltaDegrees) {
-        final Location eyeLocation = player.getEyeLocation();
-        sendRotationPacket(player, eyeLocation.getYaw(), eyeLocation.getPitch() + deltaDegrees);
-    }
-
-    public static void sendRotationPacket(Player player, double yaw, double pitch) {
-        final Location location = player.getLocation();
-        final PacketContainer packet = MANAGER.createPacket(PacketType.Play.Server.ENTITY_TELEPORT);
-        packet.getIntegers().writeSafely(0, player.getEntityId());
-        packet.getDoubles().writeSafely(0, location.getX());
-        packet.getDoubles().writeSafely(1, location.getY());
-        packet.getDoubles().writeSafely(2, location.getZ());
-        packet.getBytes().writeSafely(0, degreeToByte(yaw));
-        packet.getBytes().writeSafely(1, degreeToByte(pitch));
-        packet.getBooleans().writeSafely(0, false);
-        MANAGER.sendServerPacket(player, packet);
+    public static void changePlayerCamera(Player player, Entity entity) {
+        final PacketContainer cameraPacket = MANAGER.createPacket(PacketType.Play.Server.CAMERA);
+        cameraPacket.getIntegers().writeSafely(0, entity.getEntityId());
+        MANAGER.sendServerPacket(player, cameraPacket);
     }
 
     private static byte degreeToByte(final double degree) {
--- src/main/java/org/metamechanists/aircraft/vehicles/Vehicle.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Vehicle.java
@@ -180,7 +180,7 @@ public class Vehicle extends SlimefunItem {
         description.getCuboids(orientations).forEach((cuboidName, cuboid) -> componentGroup.getDisplays().get(cuboidName).setTransformationMatrix(cuboid.getMatrix(rotation)));
         description.updateHud(rotation, pig.getLocation().getBlockY(), hudGroup);
 
-        getPilot(pig).ifPresent(pilot -> PlayerLookHandler.sendPitchPacket(pilot, 45));
+        getPilot(pig).ifPresent(pilot -> PlayerLookHandler.changePlayerCamera(pilot, pig));
 
         if (pig.wouldCollideUsing(pig.getBoundingBox().expand(0.1, -0.1, 0.1))) {
             remove(pig, componentGroup, hudGroup);
