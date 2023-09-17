df759f29566013a1442064424075f25ed869965e
MetaMechanists <percy.baker.0@gmail.com>
2023-09-17 22:06:11 +0100

    OH NO

--- src/main/java/org/metamechanists/aircraft/vehicles/VehicleDescription.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/VehicleDescription.java
@@ -109,12 +109,10 @@ public class VehicleDescription {
     public Map<String, ModelComponent> getHud(final @NotNull Quaterniond rotation) {
         final Map<String, ModelComponent> hudComponents = new HashMap<>();
 
-        final Vector3d lookingAtForward = new Vector3d(1, 0, 0).rotate(rotation);
-        final Vector3d lookingAtForwardWithoutY = new Vector3d(lookingAtForward.x, 0, lookingAtForward.z);
-
         final Vector3d lookingAtUp = new Vector3d(0, 1, 0).rotate(rotation);
-        final double roll = lookingAtUp.angle(new Vector3d(0, 1, 0));
-        final Vector3d rollAdjustment = new Vector3d(0, 0, lookingAtUp.y > 0 ? roll : -roll);
+        final Vector3d lookingAtUpWithoutX = new Vector3d(0, lookingAtUp.y, lookingAtUp.z);
+        final double roll = lookingAtUp.angle(lookingAtUpWithoutX);
+        final Vector3d rollAdjustment = new Vector3d(0, 0, roll);
         hudComponents.put("horizon_altitude", new ModelText()
                 .background(Color.fromARGB(0, 0, 0, 0))
                 .brightness(Utils.BRIGHTNESS_ON)
@@ -131,6 +129,8 @@ public class VehicleDescription {
                 .secondRotation(rollAdjustment)
                 .facing(BlockFace.WEST));
 
+        final Vector3d lookingAtForward = new Vector3d(1, 0, 0).rotate(rotation);
+        final Vector3d lookingAtForwardWithoutY = new Vector3d(lookingAtForward.x, 0, lookingAtForward.z);
         final double adjustment = 2 * lookingAtForward.angle(lookingAtForwardWithoutY);
         final Vector3d horizonOffset = new Vector3d(0, lookingAtForward.y < 0 ? adjustment : -adjustment, 0);
         hudComponents.put("horizon_center", new ModelText()
