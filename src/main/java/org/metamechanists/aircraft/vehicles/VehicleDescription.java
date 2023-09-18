e5527484dc614c915d037b538b691b42fb54ef87
MetaMechanists <percy.baker.0@gmail.com>
2023-09-19 00:25:39 +0100

    OH NO

--- src/main/java/org/metamechanists/aircraft/vehicles/VehicleDescription.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/VehicleDescription.java
@@ -147,7 +147,7 @@ public class VehicleDescription {
                 .translate(new Vector3f(horizonOffset).add(new Vector3f(1, 1, 0)))
                 .facing(BlockFace.WEST)
                 .scale(new Vector3f(0.3F, 0.3F, 0.001F))
-                .translate(0.3F, 0.3F, 0));
+                .translate(0.5F, 0.5F, 0));
         final int bars = 61;
         final float verticalSpacing = (float) ((PI / 1.14) / (bars / 2));
         for (int i = 0; i < bars; i++) {
@@ -162,7 +162,7 @@ public class VehicleDescription {
                     .translate(new Vector3f(horizonOffset).add(new Vector3f(1, 1 - ((bars / 2) * verticalSpacing) + (verticalSpacing * i), 0)))
                     .facing(BlockFace.WEST)
                     .scale(new Vector3f(0.2F, 0.2F, 0.001F))
-                    .translate(0.2F, 0.2F, 0));
+                    .translate(0.3F, 0.3F, 0));
         }
         return hudComponents;
     }
