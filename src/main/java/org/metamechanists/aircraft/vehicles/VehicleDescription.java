bbec849a05165176add8b4e857e050006a1d2caa
MetaMechanists <percy.baker.0@gmail.com>
2023-09-19 00:13:13 +0100

    OH NO

--- src/main/java/org/metamechanists/aircraft/vehicles/VehicleDescription.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/VehicleDescription.java
@@ -129,7 +129,7 @@ public class VehicleDescription {
                 .rotate(new Vector3d(0, yaw, pitch))
                 .facing(BlockFace.WEST)
                 .scale(new Vector3f(0.7F, 0.7F, 0.001F))
-                .translate(-0.35F, -0.35F, 0));
+                .translate(0.35F, 0.35F, 0));
         hudComponents.put("horizon_aircraft", new ModelAdvancedText()
                 .text(Component.text("< = [       ] = >").color(TextColor.color(255, 255, 255)))
                 .background(Color.fromARGB(0, 0, 0, 0))
@@ -140,7 +140,7 @@ public class VehicleDescription {
                 .rotate(new Vector3d(0, yaw, pitch))
                 .facing(BlockFace.WEST)
                 .scale(new Vector3f(0.7F, 0.7F, 0.001F))
-                .translate(-0.35F, -0.35F, 0));
+                .translate(0.35F, 0.35F, 0));
 
         final float adjustment = (float) (2 * -pitch);
         final Vector3f horizonOffset = new Vector3f(0, adjustment, 0);
@@ -148,13 +148,11 @@ public class VehicleDescription {
                 .background(Color.fromARGB(0, 0, 0, 0))
                 .text(Component.text("----------------").color(TextColor.color(0, 255, 255)))
                 .brightness(Utils.BRIGHTNESS_ON)
-                .alignment(TextAlignment.CENTER)
                 .rotate(rotation)
                 .translate(new Vector3f(horizonOffset).add(new Vector3f(1, 1, 0)))
                 .facing(BlockFace.WEST)
                 .scale(new Vector3f(0.3F, 0.3F, 0.001F))
-                .translate(-0.15F, -0.15F, 0)
-                .translate(-0.075F, -0.075F, 0));
+                .translate(0.15F, 0.15F, 0));
         final int bars = 61;
         final float verticalSpacing = (float) ((PI / 1.14) / (bars / 2));
         for (int i = 0; i < bars; i++) {
@@ -165,12 +163,11 @@ public class VehicleDescription {
                     .background(Color.fromARGB(0, 0, 0, 0))
                     .text(Component.text("--------------").color(TextColor.color(0, 180, 255)))
                     .brightness(Utils.BRIGHTNESS_ON)
-                    .alignment(TextAlignment.CENTER)
                     .rotate(rotation)
                     .translate(new Vector3f(horizonOffset).add(new Vector3f(1, 1 - ((bars / 2) * verticalSpacing) + (verticalSpacing * i), 0)))
                     .facing(BlockFace.WEST)
                     .scale(new Vector3f(0.2F, 0.2F, 0.001F))
-                    .translate(-0.1F, -0.1F, 0));
+                    .translate(0.1F, 0.1F, 0));
         }
         return hudComponents;
     }
