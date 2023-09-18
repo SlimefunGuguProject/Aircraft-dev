85a4c8d52dd68ff9753e8eb32850da52abe40e90
MetaMechanists <percy.baker.0@gmail.com>
2023-09-18 13:16:13 +0100

    Revert "OH NO"
    This reverts commit 9f7de9c327e942490a7d2f09f58c51d183fe84ba.

--- src/main/java/org/metamechanists/aircraft/vehicles/Vehicle.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Vehicle.java
@@ -128,12 +128,13 @@ public class Vehicle extends SlimefunItem {
         return new Vector3d(resultantForce).div(description.getMass()).div(400);
     }
 
-    private Vector3d getAngularAcceleration(final @NotNull Set<SpatialForce> forces, final @NotNull Quaterniond rotation) {
+    private Vector3d getAngularAcceleration(final @NotNull Set<SpatialForce> forces, final @NotNull Vector3d rotation) {
         final Set<Vector3d> torqueVectors = forces.stream().map(SpatialForce::getTorqueVector).collect(Collectors.toSet());
         final Vector3d resultantTorque = new Vector3d();
         torqueVectors.forEach(resultantTorque::add);
 
-        final Quaterniond negativeRotation = new Quaterniond().rotateAxis(-rotation.angle(), rotation.x, rotation.y, rotation.z);
+        final Quaterniond rotationQuaternion = Utils.getRotation(rotation);
+        final Quaterniond negativeRotation = new Quaterniond().rotateAxis(-rotationQuaternion.angle(), rotationQuaternion.x, rotationQuaternion.y, rotationQuaternion.z);
         resultantTorque.rotate(negativeRotation);
 
         return new Vector3d(resultantTorque).div(description.getMomentOfInertia()).div(400);
@@ -163,7 +164,7 @@ public class Vehicle extends SlimefunItem {
         description.applyVelocityDampening(velocity);
         velocity.add(getAcceleration(forces));
 
-        angularVelocity = Utils.getRotation(angularVelocity.getEulerAnglesXYZ(new Vector3d())).mul(Utils.getRotation(getAngularAcceleration(forces, rotation)));
+        angularVelocity = Utils.getRotation(angularVelocity.getEulerAnglesXYZ(new Vector3d())).mul(Utils.getRotation(getAngularAcceleration(forces, rotation.getEulerAnglesXYZ(new Vector3d()))));
         angularVelocity = Utils.getRotation(description.applyAngularVelocityDampening(angularVelocity.getEulerAnglesXYZ(new Vector3d())));
         rotation.mul(Utils.getRotation(angularVelocity.getEulerAnglesXYZ(new Vector3d())));
 
