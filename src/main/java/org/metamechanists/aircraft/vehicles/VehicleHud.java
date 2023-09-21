276c83e963f15f6b80d8d3dc57e3c61027078a03
MetaMechanists <percy.baker.0@gmail.com>
2023-09-21 17:26:12 +0100

    compass time

--- src/main/java/org/metamechanists/aircraft/vehicles/VehicleHud.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/VehicleHud.java
@@ -59,7 +59,7 @@ public class VehicleHud {
                 .background(Color.fromARGB(0, 0, 0, 0))
                 .brightness(Utils.BRIGHTNESS_ON)
                 .scale(new Vector3f(0.4F, 0.4F, 0.001F))
-                .translate(0.5F, 0.5F, -0.01F);
+                .translate(1.0F, 1.0F, -0.01F);
     }
     private static ModelAdvancedText getArtificialHorizonCenter(final @NotNull Vector3f hudCenter, final @NotNull Vector3d rotation, final Vector3f horizonOffset, final boolean shouldRender) {
         return new ModelAdvancedText()
@@ -71,7 +71,7 @@ public class VehicleHud {
                 .translate(hudCenter)
                 .facing(BlockFace.WEST)
                 .scale(shouldRender ? new Vector3f(0.3F, 0.3F, 0.001F) : new Vector3f())
-                .translate(0.5F, 0.5F, 0);
+                .translate(1.0F, 1.0F, 0);
     }
     private static ModelAdvancedText getArtificialHorizonBar(final Component component,
                                                              final @NotNull Vector3f hudCenter, final @NotNull Vector3d rotation, final Vector3f horizonOffset,
@@ -86,7 +86,7 @@ public class VehicleHud {
                 .translate(barOffset)
                 .facing(BlockFace.WEST)
                 .scale(shouldRender ? new Vector3f(0.2F, 0.2F, 0.001F) : new Vector3f())
-                .translate(0.5F, 0.5F, 0);
+                .translate(1.0F, 1.0F, 0);
     }
     private static ModelAdvancedText getArtificialHorizonDegree(final Component component,
                                                                 final @NotNull Vector3f hudCenter, final @NotNull Vector3d rotation, final Vector3f horizonOffset,
