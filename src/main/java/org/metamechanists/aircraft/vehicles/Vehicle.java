8f9d0cb9800016be345c5fd88fffdebd37aafbb0
LordIdra <percy.baker.0@gmail.com>
2024-08-11 02:06:09 +0100

    add force arrows

--- src/main/java/org/metamechanists/aircraft/vehicles/Vehicle.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Vehicle.java
@@ -303,10 +303,11 @@ public class Vehicle extends SlimefunItem {
                 }
 
                 display.setTransformationMatrix(new TransformationMatrixBuilder()
-                        .translate(description.getRelativeCenterOfMass())
+                        .translate(description.getRelativeCenterOfMass().add(0.0F, 1.0F, 0.0F))
+                        .translate(0.0F, 1.1F, 0.0F)
                         .translate(new Vector3f((float) force.relativeLocation().x, (float) force.relativeLocation().y, (float) force.relativeLocation().z))
-                        .scale(0.1F, 0.1F, 0.1F * (float) force.force().length())
                         .lookAlong(new Vector3f((float) force.force().x, (float) force.force().y, (float) force.force().z))
+                        .scale(0.1F * (float) force.force().length(), 0.1F, 0.1F)
                         .buildForBlockDisplay());
             }
 
