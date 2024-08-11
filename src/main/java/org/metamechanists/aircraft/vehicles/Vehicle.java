fbf14ec4822303d624dc2eb98f8461134159828b
LordIdra <percy.baker.0@gmail.com>
2024-08-11 20:50:13 +0100

    aaaaaa

--- src/main/java/org/metamechanists/aircraft/vehicles/Vehicle.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Vehicle.java
@@ -299,8 +299,8 @@ public class Vehicle extends SlimefunItem {
 
         if (isOnGround) {
             angularAcceleration.y -= description.getGroundYawDamping() * state.angularVelocity.y;
-            relativeAngularVelocity.z -= //description.getGroundPitchDampingRotation() * state.rotation.x
-                    description.getGroundPitchDampingAngularVelocity() * relativeAngularVelocity.z;
+            relativeAngularVelocity.z -= description.getGroundPitchDampingRotation() * state.rotation.z;
+//            relativeAngularVelocity.z -= description.getGroundPitchDampingAngularVelocity() * relativeAngularVelocity.z;
         }
 
         state.rotation.set(Utils.getRotationEulerAngles(state.rotation)
