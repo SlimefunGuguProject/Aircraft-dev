d21dacab52ed7cde8d04ba58cd68f4356699cad8
LordIdra <percy.baker.0@gmail.com>
2024-08-18 14:01:59 +0100

    SWITCH ROTATION SYSTEM - this will be horrible

--- src/main/java/org/metamechanists/aircraft/utils/Utils.java
+++ src/main/java/org/metamechanists/aircraft/utils/Utils.java
@@ -22,14 +22,14 @@ public class Utils {
 
     public Matrix4f getComponentMatrix(@NotNull ModelComponent component, @NotNull Vector3d rotation) {
         Matrix3f roll = new Matrix3f(
-                (float) Math.cos(rotation.z), 0, (float) -Math.sin(rotation.z),
-                (float) Math.sin(rotation.z), 0, (float) Math.cos(rotation.z),
-                0, 1, 0
+                (float) Math.cos(rotation.z), (float) -Math.sin(rotation.z), 0,
+                (float) Math.sin(rotation.z), (float) Math.cos(rotation.z), 0,
+                0, 0, 1
         );
         Matrix3f pitch = new Matrix3f(
-                0, 1, 0,
-                (float) Math.cos(rotation.x), 0, (float) -Math.sin(rotation.x),
-                (float) Math.sin(rotation.x), 0, (float) Math.cos(rotation.x)
+                1, 0, 0,
+                0, (float) Math.cos(rotation.x), (float) -Math.sin(rotation.x),
+                0, (float) Math.sin(rotation.x), (float) Math.cos(rotation.x)
         );
         Matrix3f yaw = new Matrix3f(
                 (float) Math.cos(rotation.y), 0, (float) -Math.sin(rotation.y),
