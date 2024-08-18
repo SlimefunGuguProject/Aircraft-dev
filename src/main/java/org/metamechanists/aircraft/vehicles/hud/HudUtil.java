101be90562aa6b24edb984c15301bbc8a151fce1
LordIdra <percy.baker.0@gmail.com>
2024-08-18 17:04:50 +0100

    bump dml

--- src/main/java/org/metamechanists/aircraft/vehicles/hud/HudUtil.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/hud/HudUtil.java
@@ -51,7 +51,7 @@ public final class HudUtil {
         return defaultText()
                 .translate(hudCenter)
                 .undoRotate(state.rotation)
-//                .rotate(new Vector3d(0, -state.yaw(), state.pitch()))
+                .rotate(new Vector3d(0, state.yaw(), state.pitch()))
                 .lookAlong(BlockFace.WEST);
     }
 
