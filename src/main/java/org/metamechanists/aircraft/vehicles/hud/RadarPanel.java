dd98797d3fddf2988b39cf31c877bd1d29a81c2f
LordIdra <percy.baker.0@gmail.com>
2024-08-19 23:53:05 +0100

    radar

--- src/main/java/org/metamechanists/aircraft/vehicles/hud/RadarPanel.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/hud/RadarPanel.java
@@ -25,33 +25,33 @@ public final class RadarPanel {
                 .translate(0, 0, -offset)
                 .material(material)
                 .rotateZ(0.0)
-                .scale(new Vector3d(sideSize, diameter, 0.0001)));
+                .scale(new Vector3d(sideSize, diameter, 0.002)));
         hudComponents.put(name + "-2", HudUtil.rollIndependentCuboid(state, center)
                 .rotateY(rotation)
                 .translate(0, 0, -offset - 0.0003)
                 .material(material)
                 .rotateZ(PI / 4)
-                .scale(new Vector3d(sideSize, diameter, 0.0001)));
+                .scale(new Vector3d(sideSize, diameter, 0.004)));
         hudComponents.put(name + "-3", HudUtil.rollIndependentCuboid(state, center)
                 .rotateY(rotation)
                 .translate(0, 0, -offset - 0.0006)
                 .material(material)
                 .rotateZ(PI / 2)
-                .scale(new Vector3d(sideSize, diameter, 0.0001)));
+                .scale(new Vector3d(sideSize, diameter, 0.006)));
         hudComponents.put(name + "-4", HudUtil.rollIndependentCuboid(state, center)
                 .rotateY(rotation)
                 .translate(0, 0, -offset - 0.0009)
                 .material(material)
                 .rotateZ(PI * 3 / 4)
-                .scale(new Vector3d(sideSize, diameter, 0.001)));
+                .scale(new Vector3d(sideSize, diameter, 0.08)));
     }
 
     public static void build(VehicleState state, @NotNull Map<String, ModelComponent> hudComponents, Vector3f hudCenter) {
         Vector3f center = new Vector3f(0.0F, 0.0F, -0.4F).add(hudCenter);
         double rotation = -0.4;
 
-        addOctagon(state, hudComponents, "outer-octagon", center, rotation, Material.GREEN_STAINED_GLASS, 0.3, 0.009);
-        addOctagon(state, hudComponents, "middle-octagon", center, rotation, Material.GREEN_STAINED_GLASS, 0.2, 0.006);
-        addOctagon(state, hudComponents, "inner-octagon", center, rotation, Material.GREEN_STAINED_GLASS, 0.1, 0.003);
+        addOctagon(state, hudComponents, "outer-octagon", center, rotation, Material.GRAY_STAINED_GLASS, 0.3, 0.03);
+        addOctagon(state, hudComponents, "middle-octagon", center, rotation, Material.GREEN_STAINED_GLASS, 0.2, 0.02);
+        addOctagon(state, hudComponents, "inner-octagon", center, rotation, Material.LIME_STAINED_GLASS, 0.1, 0.01);
     }
 }
