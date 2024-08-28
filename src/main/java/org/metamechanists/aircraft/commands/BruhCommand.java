6f5958f2359c51a5c1c6be832c049a2341fb46fd
LordIdra <percy.baker.0@gmail.com>
2024-08-28 23:39:33 +0100

    bruh

--- src/main/java/org/metamechanists/aircraft/commands/BruhCommand.java
+++ src/main/java/org/metamechanists/aircraft/commands/BruhCommand.java
@@ -8,10 +8,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Display;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Transformation;
 import org.jetbrains.annotations.NotNull;
-import org.joml.AxisAngle4f;
-import org.joml.Vector3f;
 import org.metamechanists.aircraft.Aircraft;
 import org.metamechanists.displaymodellib.models.components.ModelCuboid;
 import org.metamechanists.displaymodellib.transformations.TransformationMatrixBuilder;
@@ -26,12 +23,16 @@ public class BruhCommand extends BaseCommand {
     public static void bruh(@NotNull Player player, String @NotNull [] args) {
         Display x = new ModelCuboid()
                 .material(Material.GRAY_CONCRETE)
+                .interpolationDuration(4)
                 .build(player.getLocation());
 
         Bukkit.getScheduler().scheduleSyncRepeatingTask(Aircraft.getInstance(), () -> {
+            x.setTransformationMatrix(new TransformationMatrixBuilder()
+                    .rotate(0.0, PI / 4 * Bukkit.getServer().getCurrentTick(), 0.0)
+                    .scale(1.0F, 0.2F, 0.2F)
+                    .buildForItemDisplay());
             x.setInterpolationDelay(0);
             x.setInterpolationDuration(4);
-            x.setTransformation(new Transformation(new Vector3f(), new AxisAngle4f((float) (PI / 8 * Bukkit.getServer().getCurrentTick()), 0.0F, 1.0F, 0.0F), new Vector3f(1.0F, 0.2F, 0.2F), new AxisAngle4f()));
         }, 0, 4);
     }
 }
