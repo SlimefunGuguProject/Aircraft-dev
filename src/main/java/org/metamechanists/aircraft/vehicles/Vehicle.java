b3d09906c8e1131ef2e6bf23b846e1173cce076f
LordIdra <percy.baker.0@gmail.com>
2024-08-31 12:37:46 +0100

    transformation optimisation

--- src/main/java/org/metamechanists/aircraft/vehicles/Vehicle.java
+++ src/main/java/org/metamechanists/aircraft/vehicles/Vehicle.java
@@ -221,6 +221,7 @@ public class Vehicle extends SlimefunItem {
             if (previousExpectedComponent != null) {
                 float matrixDifference = computeMatrixDifference(expectedComponent.getMatrix(), previousExpectedComponent.getMatrix());
                 if (matrixDifference < 1.0e-22) {
+                    Bukkit.getLogger().warning("bruh");
                     continue;
                 }
             }
