5ba662ece852de5c0dfcc1fe0780f443a7c773c8
LordIdra <percy.baker.0@gmail.com>
2024-10-06 22:44:24 +0100

    oops

--- src/main/java/org/metamechanists/aircraft/vehicle/component/base/TextComponent.java
+++ src/main/java/org/metamechanists/aircraft/vehicle/component/base/TextComponent.java
@@ -57,7 +57,7 @@ public abstract class TextComponent<T extends KinematicEntitySchema> extends Kin
 
         // Set text invisible
         if (!visible && display.getText() != null) {
-            display.setText("-");
+            display.setText("");
         }
 
         visibleLastLastUpdate = visibleLastUpdate;
