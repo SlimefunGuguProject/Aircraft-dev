36367cdf634657f833f2166b3a37a41f883996e0
LordIdra <percy.baker.0@gmail.com>
2024-10-11 00:52:43 +0100

    bottom panel

--- src/main/java/org/metamechanists/aircraft/vehicle/component/hud/bottompanel/QuantityBar.java
+++ src/main/java/org/metamechanists/aircraft/vehicle/component/hud/bottompanel/QuantityBar.java
@@ -18,6 +18,7 @@ import org.metamechanists.metalib.yaml.YamlTraverser;
 
 public class QuantityBar extends HudItemComponent<QuantityBar.QuantityBarSchema> {
     public static class QuantityBarSchema extends HudItemComponentSchema {
+        private final boolean foreground ;
         private final Material material;
         private final double width;
         private final double height;
@@ -26,11 +27,14 @@ public class QuantityBar extends HudItemComponent<QuantityBar.QuantityBarSchema>
         public QuantityBarSchema(
                 @NotNull String id,
                 @NotNull String prefix,
+                boolean foreground,
                 @NotNull HudSection.HudSectionSchema sectionSchema,
                 @NotNull YamlTraverser traverser
         ) {
             super(id + "_bottom_panel_" + prefix, sectionSchema, traverser, QuantityBar.class, ItemDisplay.class);
+            this.foreground = foreground;
             material = Material.getMaterial(traverser.get(prefix + "Material"));
+            assert material != null;
             height = traverser.get(prefix + "Height");
             width = traverser.get(prefix + "Width");
             offset = traverser.get(prefix + "Offset");
@@ -56,9 +60,10 @@ public class QuantityBar extends HudItemComponent<QuantityBar.QuantityBarSchema>
         Bukkit.getServer().getLogger().info(String.valueOf(schema().height));
         Bukkit.getServer().getLogger().info(String.valueOf(schema().offset));
         Bukkit.getServer().getLogger().info(String.valueOf(fraction));
+        double offsetX = -0.5F * schema().width + 0.5F * (1.0 - fraction) * schema().width;
         return schema().getSectionSchema().rollCuboid(vehicleEntity)
                 .material(schema().material)
-                .translate(-0.5F * fraction * schema().width, schema().offset, 0.05F)
+                .translate(offsetX, schema().offset, 0.5F + (schema().foreground ? 0.02F : 0.0F))
                 .scale(new Vector3f((float) schema().width * fraction, (float) schema().height, 0.001F))
                 .translate(0.5F, 0.35F, 0.0F);
     }
