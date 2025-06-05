bde37594d51445a95c366f7fc3ac0345c281b7a5
LordIdra <percy.baker.0@gmail.com>
2025-06-05 17:03:49 +0100

    ????????????????????

--- src/main/java/org/metamechanists/aircraft/vehicle/handler/MountHandler.java
+++ src/main/java/org/metamechanists/aircraft/vehicle/handler/MountHandler.java
@@ -36,7 +36,8 @@ public final class MountHandler implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     private static void onMount(@NotNull EntityMountEvent e) {
-        Bukkit.getLogger().severe("bruh");
+
+
         Entity maybePlayer = e.getEntity();
         if (!(maybePlayer instanceof Player player)) {
             return;
@@ -45,6 +46,14 @@ public final class MountHandler implements Listener {
         if (!(KinematicEntity.get(e.getMount().getUniqueId()) instanceof VehicleEntity vehicleEntity)) {
             return;
         }
+
+        new EntityMountEvent(e.getEntity(), e.getMount()) {
+            @Override
+            public void setCancelled(boolean cancel) {
+                Bukkit.getLogger().severe(Arrays.toString(Thread.currentThread().getStackTrace()));
+                super.setCancelled(cancel);
+            }
+        }.callEvent();
 
         vehicleEntity.onMount(player);
     }
