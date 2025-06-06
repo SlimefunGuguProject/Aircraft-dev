8cc919c19fa696591ee2cb64f3b526d5ff2d02d1
LordIdra <percy.baker.0@gmail.com>
2025-06-06 21:35:23 +0100

    bruh

--- src/main/java/org/metamechanists/aircraft/slimefun/Items.java
+++ src/main/java/org/metamechanists/aircraft/slimefun/Items.java
@@ -26,14 +26,11 @@ public final class Items {
     private static final String VALUE_COLOR = "<color:#2182ff>";
     private static final String UNIT_COLOR = "<color:#416a7f>";
 
-    private static final ItemGroup AIRCRAFT_GROUP = new ItemGroup(Keys.AIRCRAFT,
-            new CustomItemStack(Material.COMPASS, "&aAircraft"));
-
     private static final SlimefunItemStack THROTTLE_UP_STACK = new SlimefunItemStack(
             "AIRCRAFT_THROTTLE_UP",
             new CustomItemStack(Material.LIME_DYE, ChatColor.WHITE + "Throttle up"));
     private static final AircraftControl THROTTLE_UP = new AircraftControl(
-            AIRCRAFT_GROUP,
+            Groups.CONTROLS,
             THROTTLE_UP_STACK,
             RecipeType.NULL,
             new ItemStack[]{},
@@ -43,7 +40,7 @@ public final class Items {
             "AIRCRAFT_THROTTLE_DOWN",
             new CustomItemStack(Material.RED_DYE, ChatColor.WHITE + "Throttle down"));
     private static final AircraftControl THROTTLE_DOWN = new AircraftControl(
-            AIRCRAFT_GROUP,
+            Groups.CONTROLS,
             THROTTLE_DOWN_STACK,
             RecipeType.NULL,
             new ItemStack[]{},
@@ -53,7 +50,7 @@ public final class Items {
             "AIRCRAFT_STEER_LEFT",
             new CustomItemStack(Material.MUSIC_DISC_STAL, ChatColor.WHITE + "Steer left"));
     private static final AircraftControl STEER_LEFT = new AircraftControl(
-            AIRCRAFT_GROUP,
+            Groups.CONTROLS,
             STEER_LEFT_STACK,
             RecipeType.NULL,
             new ItemStack[]{},
@@ -63,7 +60,7 @@ public final class Items {
             "AIRCRAFT_STEER_RIGHT",
             new CustomItemStack(Material.MUSIC_DISC_STAL, ChatColor.WHITE + "Steer right"));
     private static final AircraftControl STEER_RIGHT = new AircraftControl(
-            AIRCRAFT_GROUP,
+            Groups.CONTROLS,
             STEER_RIGHT_STACK,
             RecipeType.NULL,
             new ItemStack[]{},
@@ -73,7 +70,7 @@ public final class Items {
             "AIRCRAFT_STRAFE_LEFT",
             new CustomItemStack(Material.ECHO_SHARD, ChatColor.WHITE + "Strafe left"));
     private static final AircraftControl STRAFE_LEFT = new AircraftControl(
-            AIRCRAFT_GROUP,
+            Groups.CONTROLS,
             STRAFE_LEFT_STACK,
             RecipeType.NULL,
             new ItemStack[]{},
@@ -83,7 +80,7 @@ public final class Items {
             "AIRCRAFT_STRAFE_RIGHT",
             new CustomItemStack(Material.AMETHYST_SHARD, ChatColor.WHITE + "Strafe right"));
     private static final AircraftControl STRAFE_RIGHT = new AircraftControl(
-            AIRCRAFT_GROUP,
+            Groups.CONTROLS,
             STRAFE_RIGHT_STACK,
             RecipeType.NULL,
             new ItemStack[]{},
@@ -122,12 +119,25 @@ public final class Items {
                     .build()
     );
 
+    private static final SlimefunItemStack METACOIN_UFO = new SlimefunItemStack(
+            "AIRCRAFT_METACOIN_UFO",
+            new ItemStackBuilder(Material.GOLD_INGOT)
+                    .name("Metacoin™ UFO")
+                    .loreLine(ItemStackBuilder.VEHICLE)
+                    .loreLine("")
+                    .loreLine("<color:#c9c9c9>The transportation option of the future!")
+                    .loreLine("<color:#c9c9c9>Embrace the future of blockchain technology")
+                    .loreLine("<color:#c9c9c9>TO THE MOON!!!")
+                    .loreLine("")
+                    .build()
+    );
+
     private static @NotNull VehicleItem crudeAirplane(VehicleEntitySchema schema) {
         return new VehicleItem(
                 "AIRCRAFT_CRUDE_AIRPLANE",
                 CRUDE_AIRPLANE_STACK,
                 schema,
-                AIRCRAFT_GROUP,
+                Groups.VEHICLES,
                 RecipeType.NULL,
                 new ItemStack[]{});
     }
@@ -137,7 +147,7 @@ public final class Items {
                 "AIRCRAFT_CRUDE_AIRSHIP",
                 CRUDE_AIRSHIP_STACK,
                 schema,
-                AIRCRAFT_GROUP,
+                Groups.VEHICLES,
                 RecipeType.NULL,
                 new ItemStack[]{});
     }
@@ -147,7 +157,17 @@ public final class Items {
                 "AIRCRAFT_CRUDE_DRONE",
                 CRUDE_DRONE_STACK,
                 schema,
-                AIRCRAFT_GROUP,
+                Groups.VEHICLES,
+                RecipeType.NULL,
+                new ItemStack[]{});
+    }
+
+    private static @NotNull VehicleItem metacoinUfo(VehicleEntitySchema schema) {
+        return new VehicleItem(
+                "AIRCRAFT_METACOIN_UFO",
+                METACOIN_UFO,
+                schema,
+                Groups.VEHICLES,
                 RecipeType.NULL,
                 new ItemStack[]{});
     }
@@ -193,6 +213,11 @@ public final class Items {
         if (crudeDroneSchema != null) {
             crudeDrone(crudeDroneSchema).register(addon);
         }
+
+        VehicleEntitySchema metacoinUfoSchema = loadVehicle("metacoin_ufo", METACOIN_UFO);
+        if (metacoinUfoSchema != null) {
+            metacoinUfo(metacoinUfoSchema).register(addon);
+        }
     }
 
     public static void reload() {
@@ -222,6 +247,12 @@ public final class Items {
         if (crudeDroneSchema != null) {
             SlimefunItem newItem = crudeDrone(crudeDroneSchema);
             ((VehicleItem) Slimefun.getRegistry().getSlimefunItemIds().get(newItem.getId())).schema = crudeDroneSchema;
+        }
+
+        VehicleEntitySchema metacoinUfo = loadVehicle("metacoin_ufo", METACOIN_UFO);
+        if (metacoinUfo != null) {
+            SlimefunItem newItem = metacoinUfo(metacoinUfo);
+            ((VehicleItem) Slimefun.getRegistry().getSlimefunItemIds().get(newItem.getId())).schema = metacoinUfo;
         }
     }
 
